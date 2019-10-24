be63cc621640c6962c537eab3b92b76481b1a564
akhvedelidze <akhvedelidze@gmail.com>
2019-10-24 16:56:47 -0400

    JS update

---
+++ script.js
@@ -0,0 +1,34 @@
+var laptop1 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop2 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop3 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop4 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop5 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop6 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop7 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop8 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop9 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+var laptop10 = { model: "Lenovo", operationgSystem: "windows 10", serialNumber: "KKABCC", available: true };
+
+var equipment = (laptop1, laptop2, laptop3, laptop4, laptop5, laptop6, laptop7, laptop8, laptop9, laptop10);
+
+
+
+var p1 = document.createElement("p");
+p1.innerText = laptop1.model;
+document.body.appendChild(p1);
+p1.className=
+
+var p2 = document.createElement("p");
+p2.innerText = laptop1.operationgSystem;
+document.body.appendChild(p2);
+
+var p3 = document.createElement("p");
+p3.innerText = laptop1.operationgSystem;
+document.body.appendChild(p3);
+
+
+console.log(p1);
+
+
+
+
